8893af31f43ee48c2497a98a96891c583814ec73
MaxCosmos <renyh2001@outlook.com>
2024-06-10 00:23:26 +0800

    初始化

---
+++ src/max/Test01.java
@@ -0,0 +1,10 @@
+package max;
+
+import java.util.ArrayList;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ArrayList<String> strings = new ArrayList<>();
+    }
+}
